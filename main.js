cd8469a8c5a23eda63a57628c2e2912194e3065a
WoutCherlet <cherlet.wout@gmail.com>
2021-02-04 11:25:52 +0100

    Init files

---
+++ main.js
@@ -0,0 +1,31 @@
+const electron = require('electron')
+const { app, BrowserWindow } = electron
+const url = require('url')
+const path = require('path')
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
